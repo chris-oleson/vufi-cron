f66de62396420c1b034729850f0c9fdec383ee3b
Chris Oleson <crolesonjr@gmail.com>
2024-03-02 15:40:05 -0700

    adding new route

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const axios = require('axios')
-axios.post('https://api.vufi.app/plaid/update').then(() => {
-    process.exit()
-}).catch((err) => {
-    console.log(err)
-    process.exit()
-})
+callRoutes()
+
+async function callRoutes() {
+    try {
+        await axios.post('https://api.vufi.app/plaid/update')
+        await axios.post('https://api.vufi.app/currencies/update')
+        process.exit()
+    }
+    catch {
+        process.exit()
+    }
+}
